38002429025a7bb2e07a0f54e07e353226856bf6
Антон <fistonik@yandex.ru>
2021-02-12 20:41:10 +0400

    Ветка под деплой gh-pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-// import "bootstrap/scss/bootstrap.scss";
-import App from "./components/App/App";
-
-ReactDom.render(
-  <App />,
-  document.getElementById(`root`)
-);
